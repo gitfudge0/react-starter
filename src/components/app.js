81f499f1ad508ea9643b2c8a337b4e0fe5f3e36d
thediggu <thediggu@gmail.com>
2019-03-03 13:55:52 +0530

    Add react starter app

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => (
+  <div>
+    <p>
+      React Starter App
+      <br />
+      Go bonkers!
+      <br />
+      If you want the react redux starter, checkout
+      {' '}
+      <a href="http://github.com">this project</a>
+    </p>
+  </div>
+);
+
+export default App;
